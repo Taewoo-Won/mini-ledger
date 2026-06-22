9da10ce001e601fdabed5275cdd1003eb2a9c329
Taewoo-Won <288912277+Taewoo-Won@users.noreply.github.com>
2026-06-22 11:27:30 +0000

    implement getAccount

--- Bank.java
+++ Bank.java
@@ -8,4 +8,12 @@ public class Bank {
         Account account = new Account(id, owner, initialBalance);
         accounts.put(id, account);
     }
+
+    public Account getAccount(String id) {
+        Account account = accounts.get(id);
+        if (account == null) {
+            throw new IllegalArgumentException("계좌를 찾을 수 없습니다");
+        }
+        return account;
+    }
 }
